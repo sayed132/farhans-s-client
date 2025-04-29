d953a6464dcc8421bc0946741a556ab7ed45a551
sayed132 <rafsansayed132@gmail.com>
2025-04-29 15:36:25 +0600

    firebase added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCrtxoE5z0wEtjbpOO1j31ms-X29swPlVE",
+  authDomain: "farhana-portfolio-client-fe9ab.firebaseapp.com",
+  projectId: "farhana-portfolio-client-fe9ab",
+  storageBucket: "farhana-portfolio-client-fe9ab.firebasestorage.app",
+  messagingSenderId: "953586599084",
+  appId: "1:953586599084:web:bd0ee10e5fd9e2d2e44b09"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
